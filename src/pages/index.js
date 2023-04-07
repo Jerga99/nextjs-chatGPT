4439aac34925cb21fbcf67af9cf1ccb2ca554ac2
Filip Jerga <jerga99@gmail.com>
2023-04-07 08:41:06 -0400

    Center content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,11 @@
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-full flex justify-center items-center flex-col">
+      <div>What do you want to learn?</div>
+      <div className="flex">
+        Hello I am in the center
+      </div>
+    </div>
   )
 }

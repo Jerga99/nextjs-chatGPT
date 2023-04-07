a89ab4ce75399e84ae9b967a56739873d7bec193
Filip Jerga <jerga99@gmail.com>
2023-04-07 08:54:04 -0400

    Json data and assets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 
+import stacks from "../data/stacks.json";
 
 export default function Home() {
   return (
     <div className="h-full flex justify-center items-center flex-col">
       <div>What do you want to learn?</div>
       <div className="flex">
-        Hello I am in the center
+      {JSON.stringify(stacks)}
       </div>
     </div>
   )

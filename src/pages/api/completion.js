4e1687319139587fb43b85e7af536cc7b25b7ae5
Filip Jerga <jerga99@gmail.com>
2023-04-12 12:46:40 -0400

    Make better prompt

--- src/pages/api/completion.js
+++ src/pages/api/completion.js
@@ -5,6 +5,8 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
 
+const aiPrompt = "The following is a conversation with Walt. Walt is helpful and creative. Walt's only knowledge is React JS library. He can only answer questions related to React JS. He only cares about React JS. Walt provides often code examples. Walt provides answers formated in markdown format."
+
 export default async function completion(req, res) {
   if (req.method === "POST") {
     const body = req.body;
@@ -15,9 +17,12 @@ export default async function completion(req, res) {
 
     try {
       const openai = new OpenAIApi(configuration);
+
+      const formatedPrompt = aiPrompt + "\n" + prompt + "\n" + "Walt:";
+
       const completion = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt,
+        prompt: formatedPrompt,
         temperature: 0.7,
         max_tokens: 1024
       });

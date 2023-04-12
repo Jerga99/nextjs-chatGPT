53158fd659776342278a013362299ebfd6ac7008
Filip Jerga <jerga99@gmail.com>
2023-04-12 11:03:00 -0400

    Completion API

--- src/pages/api/completion.js
+++ src/pages/api/completion.js
@@ -1,16 +1,33 @@
 
+import { Configuration, OpenAIApi } from "openai";
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
 
 export default async function completion(req, res) {
   if (req.method === "POST") {
     const body = req.body;
     const prompt = body.prompt || "";
 
-    const aiResponse = "React JS is a library for creating UIs...";
+    // const aiResponse = "React JS is a library for creating UIs...";
+    // await new Promise((res) => setTimeout(res, 500));
 
-    await new Promise((res) => setTimeout(res, 500));
+    try {
+      const openai = new OpenAIApi(configuration);
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.7,
+        max_tokens: 1024
+      });
 
-    return res.status(200).json({result: aiResponse});
+      const aiResponse = (completion.data.choices[0].text).trim();
+      return res.status(200).json({result: aiResponse});
+    } catch(e) {
+      console.log(e.message);
+      return res.status(500).json({error: {message: e.message}});
+    }
   } else {
     return res.status(500).json({error: {message: "Invalid Api Route"}})
   }

0020f7bb82c2bf37b9652d44d6da0214a9914ac6
Filip Jerga <jerga99@gmail.com>
2023-04-13 11:40:16 -0400

    Session keys

--- src/pages/learning/[stack].js
+++ src/pages/learning/[stack].js
@@ -5,6 +5,13 @@ import Message from "@/components/Message";
 import Prompt from "@/components/Prompt";
 import { useEffect, useRef, useState } from "react";
 
+const SESSION_KEYS = [
+  "u1-2023-04-13T15:36:20.424Z",
+  "u2-2023-04-13T15:36:20.123Z",
+  "u3-2023-04-13T15:36:20.421Z",
+  "u4-2023-04-13T15:36:20.999Z",
+];
+
 export default function Stack({stack, stackKey}) {
   const [messages, setMessages] = useState([]);
   const chatRef = useRef(null);
@@ -60,6 +67,18 @@ export default function Stack({stack, stackKey}) {
   return (
     <div className="h-full flex flex-col">
       <Header logo={stack.logo} info={stack.info} />
+      <select 
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-[200px] p-2.5 mt-5"
+      >
+        <option value={""}>
+          Choose session
+        </option>
+        { SESSION_KEYS.map((sk) =>
+          <option key={sk} value={sk}>
+            {sk}
+          </option>
+        )}
+      </select>
       <hr className="my-4" />
       <div ref={chatRef} className="chat flex flex-col h-full overflow-scroll">
         { messages.map((message, i) =>
